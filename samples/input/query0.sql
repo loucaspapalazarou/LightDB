8bd541302c93c8f583ee5e35794dc0a7501cf2d2
Loukas <loukas.papalazarou.cy@gmail.com>
2024-01-30 22:42:23 +0000

    will work soon

--- samples/input/query0.sql
+++ samples/input/query0.sql
@@ -1,2 +1,2 @@
 SELECT * FROM Sailors WHERE Sailors.B > 200;
--- SELECT * FROM Sailors WHERE 1 < 2 AND 3 = 17
+-- SELECT * FROM Sailors WHERE 10 > 2
--- src/main/java/ed/inf/adbs/lightdb/evaluators/ExpressionVisitor.java
+++ src/main/java/ed/inf/adbs/lightdb/evaluators/ExpressionVisitor.java
@@ -1,12 +1,14 @@
 package ed.inf.adbs.lightdb.evaluators;
 
 import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.StringValue;
-import net.sf.jsqlparser.expression.operators.arithmetic.Addition;
+import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
 import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThanEquals;
 import net.sf.jsqlparser.expression.operators.relational.MinorThan;
+import net.sf.jsqlparser.expression.operators.relational.MinorThanEquals;
+import net.sf.jsqlparser.expression.operators.relational.NotEqualsTo;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.util.deparser.ExpressionDeParser;
 import ed.inf.adbs.lightdb.catalog.DatabaseCatalog;
@@ -14,31 +16,90 @@ import ed.inf.adbs.lightdb.types.Tuple;
 
 public class ExpressionVisitor extends ExpressionDeParser {
     private DatabaseCatalog catalog;
-    private boolean result;
     private Tuple tuple;
+    private boolean result;
 
     public ExpressionVisitor(Tuple tuple, DatabaseCatalog catalog) {
         this.catalog = catalog;
         this.tuple = tuple;
+        this.result = true;
+    }
+
+    public boolean getResult() {
+        return this.result;
+    }
+
+    private void updateResult(boolean expr) {
+        this.result &= expr;
+    }
+
+    private LongValue columnReferenceToLongValue(Column column, Tuple tuple, DatabaseCatalog catalog) {
+        String columnName = column.getColumnName();
+        String tableName = column.getTable().getName();
+        int idx = catalog.getColumnIndex(tableName, columnName);
+        return new LongValue().withValue(tuple.getValueAt(idx));
+    }
+
+    @Override
+    public void visit(EqualsTo equalsTo) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visit(NotEqualsTo notEqualsTo) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visit(MinorThan minorThan) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visit(MinorThanEquals minorThanEquals) {
+        throw new UnsupportedOperationException();
+    }
+
+    // TODO: Abstract this method and just change the operator on the bottom
+    // then just implement the rest of the methods here.
+    @Override
+    public void visit(GreaterThan greaterThan) {
+        Expression left = greaterThan.getLeftExpression();
+        Expression right = greaterThan.getRightExpression();
+
+        LongValue leftValue, rightValue;
+
+        if (left instanceof Column) {
+            leftValue = columnReferenceToLongValue((Column) left, this.tuple, this.catalog);
+        } else {
+            leftValue = (LongValue) left;
+        }
+
+        if (right instanceof Column) {
+            rightValue = columnReferenceToLongValue((Column) right, this.tuple, this.catalog);
+        } else {
+            rightValue = (LongValue) right;
+        }
+        updateResult(leftValue.getValue() > rightValue.getValue());
+    }
+
+    @Override
+    public void visit(GreaterThanEquals greaterThanEquals) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visit(AndExpression andExpression) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void visit(Column column) {
-        referenceColumnToNumericColumn(column, tuple, catalog);
+        throw new UnsupportedOperationException();
     }
 
-    public boolean getResult() {
-        return result;
-    }
-
-    private Column referenceColumnToNumericColumn(Column column, Tuple tuple, DatabaseCatalog catalog) {
-        String columnName = column.getColumnName();
-        String tableName = column.getTable().getName();
-        System.out.println(tableName + " " + columnName);
-
-        int idx = catalog.getColumnIndex(tableName, columnName);
-        int value = tuple.getValueAt(idx);
-        System.out.println(value);
-        return null;
+    @Override
+    public void visit(LongValue longValue) {
+        throw new UnsupportedOperationException();
     }
 }
